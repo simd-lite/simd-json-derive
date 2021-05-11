0919a4e2452dffb00165e61a61ecaa51ffc19e7c
Heinz N. Gies <heinz@licenser.net>
2021-05-11 12:57:42 +0200

    Add tests for borrow
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tests/attrs.rs
+++ tests/attrs.rs
@@ -1,23 +1,29 @@
-use simd_json_derive::Serialize;
+use simd_json_derive::{Deserialize, Serialize};
 
 #[test]
 fn rename() {
-    #[derive(simd_json_derive::Serialize)]
-    struct Bla {
+    #[derive(Serialize, Deserialize)]
+    struct Bla<'f3> {
         #[serde(rename = "f3")]
         f1: u8,
         #[simd_json(rename = "f4")]
         f2: String,
+        #[serde(borrow)]
+        f3: &'f3 str,
         f5: u8,
     }
 
     let b = Bla {
         f1: 1,
         f2: "snot".into(),
+        f3: "snot",
         f5: 8,
     };
     println!("{}", b.json_string().unwrap());
-    assert_eq!(r#"{"f3":1,"f4":"snot","f5":8}"#, b.json_string().unwrap())
+    assert_eq!(
+        r#"{"f3":1,"f4":"snot","f3":"snot","f5":8}"#,
+        b.json_string().unwrap()
+    )
 }
 
 #[test]
